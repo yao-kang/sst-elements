0584d7c76fc7ce18126c04733be2698a2d0aad6d
nmhamster <sdhammo@sandia.gov>
2016-10-17 19:13:24 -0600

    Initial Vanadis Core test

---
+++ src/sst/elements/vanadis/test/test-simple-vcore.py
@@ -0,0 +1,42 @@
+import sst
+
+# Define SST core options
+sst.setProgramOption("stopAtCycle", "0 ns")
+
+vcore = sst.Component("vcore0", "vanadis.VanadisCore")
+vcore.addParams( {
+	"verbose" : 8,
+	"coreid"  : 0,
+	"clock"   : "1.0GHz"
+})
+
+comp_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+comp_l1cache.addParams({
+      "access_latency_cycles" : "2",
+      "cache_frequency" : "2.4 GHz",
+      "replacement_policy" : "lru",
+      "coherence_protocol" : "MESI",
+      "associativity" : "4",
+      "cache_line_size" : "64",
+      "prefetcher" : "cassini.StridePrefetcher",
+      "debug" : "1",
+      "L1" : "1",
+      "cache_size" : "32KB"
+})
+
+comp_memory = sst.Component("memory", "memHierarchy.MemController")
+comp_memory.addParams({
+      "coherence_protocol" : "MESI",
+      "backend.access_time" : "100 ns",
+      "backend.mem_size" : "4096MiB",
+      "clock" : "1GHz"
+})
+
+# Define the simulation links
+link_cpu_cache_link = sst.Link("link_cpu_cache_link")
+link_cpu_cache_link.connect( (vcore, "icache_link", "1000ps"), (comp_l1cache, "high_network_0", "1000ps") )
+link_cpu_cache_link.setNoCut()
+
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (comp_l1cache, "low_network_0", "50ps"), (comp_memory, "direct_link", "50ps") )
+
