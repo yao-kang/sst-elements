09c9ffd493247870988521364fe4cc567400e789
Arun Rodrigues <afrodri@sandia.gov>
2019-07-01 13:28:28 -0600

    first part of laoder

--- src/sst/elements/mips_4kc/read-aout.cc
+++ src/sst/elements/mips_4kc/read-aout.cc
@@ -24,6 +24,10 @@
 
 
 #include <sst_config.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <libelf/gelf.h>
 #include "mips_4kc.h"
 
 using namespace SST;
@@ -51,6 +55,84 @@ int MIPS4KC::read_aout_file (const char *file_name)
 {
     // load instructions, pre-decoded 
     text_dir = 1;
+    
+    if (elf_version(EV_CURRENT) == EV_NONE) {
+        out.fatal(CALL_INFO,-1, "ELF library initialization failed: %s\n", 
+                  elf_errmsg(-1));
+    }
+
+    int fd;
+    if ((fd = open("test/a.out", O_RDONLY, 0)) < 0) 
+        out.fatal(CALL_INFO,-1, "ELF file open failed\n");
+
+    Elf *e;
+    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL) 
+        out.fatal(CALL_INFO,-1,"elf_begin() failed: %s.",elf_errmsg(-1));
+
+    if (elf_kind(e) != ELF_K_ELF)
+        out.fatal(CALL_INFO,-1, "file is not an ELF object.");
+
+    Elf32_Ehdr *ehdr;
+    if ((ehdr = elf32_getehdr(e)) == NULL) {
+        out.fatal(CALL_INFO,-1, "getehdr() failed: %s.",elf_errmsg(-1));
+        if (ehdr->e_machine != EM_MIPS) {
+            out.fatal(CALL_INFO,-1, "Executable is not MIPS\n");
+        }
+        if (ehdr->e_type != ET_EXEC) {
+            out.fatal(CALL_INFO,-1, "File is not Executable\n");
+        }
+        if (ehdr->e_ident[EI_CLASS] != ELFCLASS32) {
+            out.fatal(CALL_INFO,-1, "Exdecutable is not 32-bit\n");
+        }
+        if (ehdr->e_ident[EI_DATA] != ELFDATA2LSB) {
+            out.fatal(CALL_INFO,-1, "Exdecutable is LSB Endianness\n");
+        }
+    }
+
+    size_t shstrndx;
+    if (elf_getshdrstrndx(e, &shstrndx) != 0) 
+        out.fatal(CALL_INFO,-1, "elf getshdrstrndx() failed:␣%s.", elf_errmsg(-1));
+
+    Elf_Scn *scn=NULL;
+    GElf_Shdr shdr;
+    while ((scn = elf_nextscn(e, scn)) != NULL) {
+        if (gelf_getshdr(scn, &shdr) != &shdr) 
+            out.fatal(CALL_INFO,-1 , "getshdr() failed:␣%s.", elf_errmsg(-1));
+
+        char *name;
+        if ((name = elf_strptr(e, shstrndx , shdr.sh_name)) == NULL)
+            out.fatal(CALL_INFO,-1 , "elf_strptr() failed:␣%s.", 
+                      elf_errmsg(-1));
+        
+        printf("Section %4lu %s Size=%lu Flags= %s %s %s\n", 
+               elf_ndxscn(scn), name, shdr.sh_size,
+               (shdr.sh_flags & SHF_WRITE) ? "Write " : "",
+               (shdr.sh_flags & SHF_ALLOC) ? "Alloc " : "",
+               (shdr.sh_flags & SHF_EXECINSTR) ? "Exec " : "");
+        
+        // load executable sections
+        if (shdr.sh_flags & SHF_EXECINSTR) {
+            Elf_Data *data=NULL;
+            size_t n=0;
+            while (n < shdr.sh_size &&
+                   (data = elf_getdata(scn, data)) != NULL) {
+                printf(" Data: align %d, off %d, size %d\n", data->d_align, 
+                       data->d_off, data->d_size);
+                //pointer to instruction
+                uint32_t* wp = (uint32_t*) data->d_buf;
+                // make instruction
+                instruction *add_inst = inst_decode(*wp); 
+                // TODO: figure out how to store_instruction to arbitrary address, store instruction there
+                // TODO: if .init section, set the 
+                // set program_starting_address
+            }
+        }
+    }
+
+    elf_end(e);
+    close(fd);
+
+#if 0
     //text_begins_at_point(0x1000); //(header.text_start);
     //store_instruction(inst_decode (getw (fd)));
     instruction *add_inst = inst_decode(0x0221820); // ADD r3, r1, r2
@@ -68,11 +150,13 @@ int MIPS4KC::read_aout_file (const char *file_name)
      add_inst = inst_decode(0x0221820); // ADD r3, r1, r2
     store_instruction(add_inst);
 
-     add_inst = inst_decode(0x0221820); // ADD r3, r1, r2
+    add_inst = inst_decode(0x0221820); // ADD r3, r1, r2
     store_instruction(add_inst);
 
     printf("Reading...\n");
     print_inst(TEXT_BOT);
+#endif
+
     text_dir = 0;
     program_starting_address = TEXT_BOT; //header.entry;
     // load data
