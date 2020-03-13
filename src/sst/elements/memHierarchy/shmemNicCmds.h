0ae73142d0e5344251023e31e6d15a6e29cf5143
Levenhagen <mjleven@s1043496.srn.sandia.gov>
2020-03-13 15:14:23 -0600

    some changes

--- src/sst/elements/memHierarchy/shmemNicCmds.h
+++ src/sst/elements/memHierarchy/shmemNicCmds.h
@@ -1,22 +1,23 @@
 struct NicCmd {
     enum Type { Shmem=1, Fam } type;
-    uint64_t respAddr;
     uint32_t handle;
+    uint32_t numData;
+    uint64_t respAddr;
     union Data {
         struct Shmem {
-            enum Op { Quiet=1, Inc }   op;
+            enum Op { Quiet=1, Inc } op;
             int pe;
             uint64_t srcAddr;
             uint64_t destAddr;
         } shmem;
         struct Fam {
-            int op;
+            enum Op { Get=1, Put  } op;
             int srcPid;
             int destNode;
             uint64_t destAddr;
-            uint64_t value;
         } fam;
     } data;
+    uint64_t value;
     uint64_t pad;
 };
 
